a71a8acb882c26573a5e6e1696db76669851c577
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-19 11:53:35 -0300

    up: fundamentos variavel e constante

--- exercicios/src/fundamentos/PrimeiroProjeto.java
+++ exercicios/src/fundamentos/PrimeiroProjeto.java
@@ -2,6 +2,6 @@ package fundamentos;
 
 public class PrimeiroProjeto {
 	public static void main(String[] args) {
-		System.out.println("Primeiro Projeito!");
+		System.out.println("Primeiro Projeto!");
 	}
 }
