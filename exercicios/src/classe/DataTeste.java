46b913daf51168788b5d1500d3d52b628ee4e1c6
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-21 18:27:44 -0300

    up: desafio data

---
+++ exercicios/src/classe/DataTeste.java
@@ -0,0 +1,13 @@
+package classe;
+
+public class DataTeste {
+	public static void main(String[] args) {
+
+		Data d = new Data();
+		d.dia = 11;
+		d.mes = 03;
+		d.ano = 1987;
+
+		System.out.printf("%d / %d / %d.", d.dia, d.mes, d.ano);
+	}
+}
