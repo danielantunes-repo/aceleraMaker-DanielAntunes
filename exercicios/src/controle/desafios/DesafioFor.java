59ec1efc975bfa2f3a7546bd4b03d2f46dec6abb
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-21 10:38:54 -0300

    up: desafio for somente com string

---
+++ exercicios/src/controle/desafios/DesafioFor.java
@@ -0,0 +1,10 @@
+package controle.desafios;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+
+		for (String valor = "#"; !valor.equals("######"); valor += "#") {
+			System.out.println(valor);
+		}
+	}
+}
