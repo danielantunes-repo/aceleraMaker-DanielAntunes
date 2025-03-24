fa1c8057e8b87f53e506af267fbbacba0c1e4d94
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-24 11:50:22 -0300

    up: desafio construtor Data

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,6 +5,19 @@ public class Data {
 	int mes;
 	int ano;
 	
+	Data(){
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	
+	Data(int diaFornecido, int mesFornecio, int anoFornecido) {
+		dia = diaFornecido;
+		mes = mesFornecio;
+		ano = anoFornecido;
+	}
+	
+	
 	String obterDataFormatada() {
 //		return dia + "/" + mes + "/" + ano;
 		return String.format("%d/%d/%d", dia, mes, ano);
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -3,12 +3,16 @@ package classe;
 public class DataTeste {
 	public static void main(String[] args) {
 
-		Data d = new Data();
-		d.dia = 11;
-		d.mes = 03;
-		d.ano = 1987;
+		Data d = new Data(11, 03, 1987);
+//		d.dia = 11;
+//		d.mes = 03;
+//		d.ano = 1987;
+		
+		Data d2 = new Data();
+		
 
 //		System.out.printf("%d / %d / %d.", d.dia, d.mes, d.ano);
 		System.out.println(d.obterDataFormatada());
+		System.out.println(d2.obterDataFormatada());
 	}
 }
