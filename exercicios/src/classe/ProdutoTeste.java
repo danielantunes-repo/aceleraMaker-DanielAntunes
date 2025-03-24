30a941837bbfbe768c97253d79d261f5d57b9672
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-24 14:47:11 -0300

    up

--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -2,15 +2,15 @@ package classe;
 
 public class ProdutoTeste {
 	public static void main(String[] args) {
+		Produto.desconto = 0.50;
 
-		Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+		Produto p1 = new Produto("Notebook", 4356.89);
 
 		double precoFinal1 = p1.precoComDesconto();
 
 		var p2 = new Produto();
 		p2.nome = "Caneta";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
 
 		double precoFinal2 = p2.precoComDesconto(0.1);
 
@@ -20,5 +20,7 @@ public class ProdutoTeste {
 		System.out.println(p2.nome);
 		System.out.println(p2.preco);
 		System.out.printf("Preço com desconto %.2f", precoFinal2);
+		
+		
 	}
 }
