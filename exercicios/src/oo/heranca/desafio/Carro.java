f1327ea6075068eb0c58929c9a6027b1f736a2eb
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-27 10:30:29 -0300

    up: interface

--- exercicios/src/oo/heranca/desafio/Carro.java
+++ exercicios/src/oo/heranca/desafio/Carro.java
@@ -2,30 +2,40 @@ package oo.heranca.desafio;
 
 public class Carro {
 
-	final int VELOCIDADE_MAXIMA;
-	int velocidadeAtual;
-	int delta = 5;
+	public final int VELOCIDADE_MAXIMA;
+	protected int velocidadeAtual;
+	private int delta = 5;
 
 	protected Carro(int velocidadeMaxima) {
 		VELOCIDADE_MAXIMA = velocidadeMaxima;
 	}
 
 	public void acelerar() {
-		if (velocidadeAtual + delta > VELOCIDADE_MAXIMA) {
+		if (velocidadeAtual + getDelta() > VELOCIDADE_MAXIMA) {
 			velocidadeAtual = VELOCIDADE_MAXIMA;
 		} else {
-			velocidadeAtual += delta;
+			velocidadeAtual += getDelta();
 		}
 
 	}
 
 	public void frear() {
-		if (velocidadeAtual >= delta) {
+		if (velocidadeAtual >= 5) {
 			velocidadeAtual -= 5;
 		} else {
 			velocidadeAtual = 0;
 		}
 
+	}
+	
+	
+
+	public int getDelta() {
+		return delta;
+	}
+
+	public void setDelta(int delta) {
+		this.delta = delta;
 	}
 
 	public String toString() {
--- exercicios/src/oo/heranca/teste/Teste.java
+++ exercicios/src/oo/heranca/teste/Teste.java
@@ -7,15 +7,19 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		Gol c = new Gol();
-		Ferrari f = new Ferrari();
+		Ferrari f = new Ferrari(400);
 		
 		
-		c.acelerar();
-		c.frear();
-		c.frear();
+//		c.acelerar();
+//		c.frear();
+//		c.frear();
+//		f.acelerar();
+		f.ligarTurbo();
+		f.ligarAr();
 		f.acelerar();
+		f.frear();
 		
-		System.out.println(c);
+//		System.out.println(c);
 		System.out.println(f);
 		
 		
