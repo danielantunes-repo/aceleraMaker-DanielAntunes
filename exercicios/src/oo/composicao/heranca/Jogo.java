82df3a4e5da07dd432b1d13b72b4218350fe0ea7
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-26 22:13:58 -0300

    up

--- exercicios/src/oo/composicao/heranca/Jogo.java
+++ exercicios/src/oo/composicao/heranca/Jogo.java
@@ -7,9 +7,8 @@ public class Jogo {
 		monstro.x = 10;
 		monstro.y = 10;
 		
-		Heroi heroi = new Heroi();
-		heroi.x = 10;
-		heroi.y = 11;
+		Heroi heroi = new Heroi(10, 11);
+		
 		
 //		j1.andar(Direcao.NORTE);
 //		j1.andar(Direcao.LESTE);
