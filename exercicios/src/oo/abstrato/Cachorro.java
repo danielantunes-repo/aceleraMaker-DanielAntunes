c60275df580f92fd03e67c7d664c909c11601530
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-27 11:00:42 -0300

    up: abstract

---
+++ exercicios/src/oo/abstrato/Cachorro.java
@@ -0,0 +1,18 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero{
+	
+	@Override
+	public String mover() {
+		
+		return "Usandoa as patas";
+	}
+	
+	@Override
+	public String mamar() {
+		// TODO Auto-generated method stub
+		return "Usando leite";
+	}
+	
+	
+}
