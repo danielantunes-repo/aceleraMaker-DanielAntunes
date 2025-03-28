77f7624563872e2aa78dde09116fbef31bfbabbf
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-28 15:57:41 -0300

    up: desafio filter

---
+++ exercicios/src/streams/desafios/CarroDesafioFilter.java
@@ -0,0 +1,15 @@
+package streams.desafios;
+
+public class CarroDesafioFilter {
+
+	final String nome;
+	final double preco;
+	final boolean arCondicionado;
+
+	public CarroDesafioFilter(String nome, double preco, boolean arCondicionado) {
+		this.nome = nome;
+		this.preco = preco;
+		this.arCondicionado = arCondicionado;
+	}
+
+}
---
+++ exercicios/src/streams/desafios/DesafioFilter.java
@@ -0,0 +1,33 @@
+package streams.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class DesafioFilter {
+
+	public static void main(String[] args) {
+		
+		CarroDesafioFilter c1 = new CarroDesafioFilter("Uno", 15000.00, false);
+		CarroDesafioFilter c2 = new CarroDesafioFilter("Gol", 25000.00, false);
+		CarroDesafioFilter c3 = new CarroDesafioFilter("HB20", 55000.00, true);
+		CarroDesafioFilter c4 = new CarroDesafioFilter("Palio", 23000.00, false);
+		CarroDesafioFilter c5 = new CarroDesafioFilter("Corolla", 150000.00, true);
+		
+		List<CarroDesafioFilter> carros = Arrays.asList(c1, c2, c3, c4, c5);
+		
+		Predicate<CarroDesafioFilter> carrosBaratos = c -> c.preco >= 50000;
+		Predicate<CarroDesafioFilter> temAr = c -> c.arCondicionado;
+		
+		Function<CarroDesafioFilter, String> carroComAr =
+				c -> "O " + c.nome + " tem o valor de mercado de R$" + c.preco + " e tem ar condicionado";
+		
+		
+		carros.stream()
+			.filter(carrosBaratos)
+			.filter(temAr)
+			.map(carroComAr)
+			.forEach(System.out::println);
+	}
+}
