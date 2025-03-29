83b1d55563fa23751d23333bde071fbc503c7cd2
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-03-29 15:00:24 -0300

    up: excecão personalizada 2

---
+++ exercicios/src/excessao/personalizadaB/StringVaziaException.java
@@ -0,0 +1,14 @@
+package excessao.personalizadaB;
+
+@SuppressWarnings("serial")
+public class StringVaziaException extends RuntimeException {
+	private String nomeDoAtributo;
+	
+	public StringVaziaException(String nomeDoAtributo) {
+		this.nomeDoAtributo = nomeDoAtributo;
+	}
+	
+	public String getMessage() {
+		return String.format("O atributo '%s' esta vazio", nomeDoAtributo);
+	}
+}
