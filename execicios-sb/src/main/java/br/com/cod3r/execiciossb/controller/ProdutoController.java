a5980305331f64dc7edacedcce877bad91aa7b0a
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-04-03 16:43:12 -0300

    up

--- execicios-sb/src/main/java/br/com/cod3r/execiciossb/controller/ProdutoController.java
+++ execicios-sb/src/main/java/br/com/cod3r/execiciossb/controller/ProdutoController.java
@@ -1,6 +1,10 @@
 package br.com.cod3r.execiciossb.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +24,18 @@ public class ProdutoController {
 	@PostMapping
 	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
-		
 		return produto;
 	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path="/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+	
+	
 }
