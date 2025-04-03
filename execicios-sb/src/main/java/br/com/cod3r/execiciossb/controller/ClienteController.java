dfba682530707c07eb61543dc36816e0e84005ad
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-04-03 10:32:08 -0300

    up: paramentro

--- execicios-sb/src/main/java/br/com/cod3r/execiciossb/controller/ClienteController.java
+++ execicios-sb/src/main/java/br/com/cod3r/execiciossb/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.cod3r.execiciossb.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.execiciossb.model.Cliente;
@@ -15,9 +16,14 @@ public class ClienteController {
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
-	
+
 	@GetMapping("/{id}")
 	public Cliente obterClientePorId(@PathVariable int id) {
 		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id") int id) {
+		return new Cliente(id, "Jorge", "123.654.321-00");
 	}
 }
