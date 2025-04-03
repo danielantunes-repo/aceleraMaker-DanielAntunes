690ae75e6ddee7239840da627d83f89ad9bd9880
DanielAntunes-dev <dap.junioruvv@gmail.com>
2025-04-03 16:34:22 -0300

    up

--- execicios-sb/src/main/java/br/com/cod3r/execiciossb/controller/ProdutoController.java
+++ execicios-sb/src/main/java/br/com/cod3r/execiciossb/controller/ProdutoController.java
@@ -3,12 +3,12 @@ package br.com.cod3r.execiciossb.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.execiciossb.model.entities.Produto;
 import br.com.cod3r.execiciossb.model.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -18,8 +18,7 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 	
 	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		
 		return produto;
